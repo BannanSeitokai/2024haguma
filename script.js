aad49dff73b4428c66c866f2d83ef80f00abfe7f
BannanSeitokai <bannnanseitokai@gmail.com>
2024-03-08 16:17:39 +0900

    update

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ const desks2 = document.getElementById('desks2');
 const desks2img = document.getElementById('desks2img');
 const desks3 = document.getElementById('desks3');
 const desks3img = document.getElementById('desks3img');
+const cardboard = document.getElementById('cardboard');
+const cardboardimg = document.getElementById('cardboardimg');
 
 area.addEventListener('click', function() { areaimg.classList.toggle('hidden'); });
 desks2.addEventListener('click', function() { desks2img.classList.toggle('hidden'); });
 desks3.addEventListener('click', function() { desks3img.classList.toggle('hidden'); });
+cardboard.addEventListener('click', function() { cardboardimg.classList.toggle('hidden'); });
